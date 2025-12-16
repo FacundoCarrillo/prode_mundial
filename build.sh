51bdf0fb593d4510e3a499fa9e26871ff6da2b90
FacundoCarrillo <facundoncarrilloutn@gmail.com>
2025-12-16 20:53:45 -0300

    Automatizando admin y partidos en el build

--- build.sh
+++ build.sh
@@ -6,3 +6,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+# 1. Crear Superusuario Automático (Si no existe)
+echo "Creando superusuario..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+
+# 2. Actualizar Partidos Automáticamente
+echo "Actualizando partidos desde la API..."
+python manage.py actualizar_partidos
